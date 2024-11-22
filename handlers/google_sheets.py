3466ddb44b42d8593de68ad102ad1459af5dcb20
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-22 13:19:16 -0800

    feat(sheets handler): append_sheet

--- handlers/google_sheets.py
+++ handlers/google_sheets.py
@@ -96,3 +96,37 @@ def get_spreadsheet_data(credentials, spreadsheet_id, range_name):
     except HttpError as error:
         print(f"An error occurred: {error}")
         return error
+
+
+def append_sheet(credentials, spreadsheet_id, sheet_name, data):
+    print(data)
+    try:
+        service = build("sheets", "v4", credentials=credentials)
+
+        result = (
+            service.spreadsheets()
+            .values()
+            .get(spreadsheetId=spreadsheet_id, range=f"{sheet_name}!1:1")
+            .execute()
+        )
+
+        header = result.get("values", [])[0]
+
+        body = {"values": [[data.get(field, None) for field in header]]}
+        result = (
+            service.spreadsheets()
+            .values()
+            .append(
+                spreadsheetId=spreadsheet_id,
+                range=sheet_name,
+                valueInputOption="USER_ENTERED",
+                body=body,
+            )
+            .execute()
+        )
+        print(f"{(result.get('updates').get('updatedCells'))} cells appended.")
+        return result
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
